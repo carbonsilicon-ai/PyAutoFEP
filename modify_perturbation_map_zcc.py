b9ab3626230489c1f2305681de872d93b4a6c6b1
Yufeng.Pan <panyvfeng@gmail.com>
2023-07-05 07:52:58 +0000

    生成和修改微扰图

--- modify_perturbation_map_zcc.py
+++ modify_perturbation_map_zcc.py
@@ -1,34 +1,33 @@
-import rdkit.Chem
-import rdkit.Chem.PropertyMol
-from merge_topologies_zcc import find_mcs, constrained_two_mol
-import pickle
-import os
+from merge_topologies_zcc import find_mcs
 import savestate_util
-import networkx
 from generate_perturbation_map_atom_map_wangwei import fill_thermograph
 import json
 import argparse
-def Modify_Map(data):
+import process_user_input
+
+
+def Modify_Map(modify_data, progress_file, graph_file):
     try:
-        add_edge = data['add_edge']
+        add_edge = modify_data['add_edge']
     except:
         add_edge = []
     try:
-        del_edge = data['del_edge']
+        del_edge = modify_data['del_edge']
     except:
         del_edge = []
     # read the progress.pkl and modify the perturbation map and corresponding atom maps
-    progress_data = savestate_util.SavableState('progress.pkl')
+    progress_data = savestate_util.SavableState(progress_file)
 
     new_graph = progress_data['thermograph']['last_solution']['best_solution']
     molecules_dict = progress_data['ligands_data']
     bias = progress_data['thermograph']['last_solution']['bias']
 
-    perturbation_map = {(node_i, node_j): edge_data for node_i, node_j, edge_data in new_graph.edges.data()}
+    perturbation_map = {(node_i, node_j): edge_data for node_i,
+                        node_j, edge_data in new_graph.edges.data()}
 
     # add edges
     add_edge_mcs = []
-    if len(add_edge)> 0 or add_edge !=None:
+    if len(add_edge) > 0 or add_edge != None:
         for a_edge in add_edge:
             mcs = find_mcs([molecules_dict[a_edge[0]]['molecule'], molecules_dict[a_edge[1]]['molecule']],
                            matchValences=True, ringMatchesRingOnly=True, completeRingsOnly=True,
@@ -45,7 +44,8 @@ def Modify_Map(data):
         # calculate the thermgraph of added edges
         # full_thermograph = networkx.DiGraph()
 
-        molecules_dict_mol = {k: v['molecule'] for k, v in molecules_dict.items()}
+        molecules_dict_mol = {k: v['molecule']
+                              for k, v in molecules_dict.items()}
         print('molecules_dict_mol: ', molecules_dict_mol)
 
         new_graph = fill_thermograph(new_graph, molecules_dict_mol, pairlist=add_edge_mcs, use_hs=False,
@@ -55,19 +55,19 @@ def Modify_Map(data):
         # print('full_thermograph', new_graph)
         print('perturbation_graph_edges:', new_graph.edges.data())
         print('perturbation_graph_nodes:', new_graph.nodes.data())
-        perturbation_map_add = {(node_i, node_j): edge_data for node_i, node_j, edge_data in new_graph.edges.data()}
+        perturbation_map_add = {
+            (node_i, node_j): edge_data for node_i, node_j, edge_data in new_graph.edges.data()}
 
         # merge previous perturbation map and added perturbation map
         perturbation_map = {**perturbation_map, **perturbation_map_add}
         print('merged_perturbation_map_added: ', perturbation_map)
 
-
     # delete edges
     if len(del_edge) > 0 or del_edge != None:
         for de in del_edge:
             de = tuple(de)
-            #print('de',de)
-            #print('del_edge:',del_edge)
+            # print('de',de)
+            # print('del_edge:',del_edge)
             if de not in perturbation_map.keys():
                 print("Warning: edge {} does not exist.".format(de))
             else:
@@ -79,7 +79,8 @@ def Modify_Map(data):
         disconnected_ligands = [lig for lig in molecules_dict if
                                 lig not in [k for tup in perturbation_map.keys() for k in tup]]
         if disconnected_ligands:
-            print("Warning: ligands {} are not connect to any other ligands.".format(disconnected_ligands))
+            print("Warning: ligands {} are not connect to any other ligands.".format(
+                disconnected_ligands))
             if continue_delete:
                 for dis_lig in disconnected_ligands:
                     new_graph.remove_node(dis_lig)
@@ -105,57 +106,48 @@ def Modify_Map(data):
     center_molecule = bias
     outer_edges = deepcopy(new_graph)
     outer_edges.remove_node(center_molecule)
-    node_position = networkx.drawing.circular_layout(outer_edges, center=[0.0, 0.0])
+    node_position = networkx.drawing.circular_layout(
+        outer_edges, center=[0.0, 0.0])
     node_position[center_molecule] = [0.0, 0.0]
     print('full_thermograph:', new_graph)
     print('pos:', node_position)
     networkx.drawing.draw(new_graph, with_labels=True, pos=node_position)
-    matplotlib.pyplot.savefig('best_graph_modified.svg')
+    matplotlib.pyplot.savefig(graph_file)
 
-def output_info_json():
+
+def output_info_json(progress_file, map_info_file):
     import pickle
     from rdkit import Chem
 
-    input_data = 'progress.pkl'
-    file_path = 'modified_map_info.json'
-
-    with open(input_data, 'rb') as fh:
+    with open(progress_file, 'rb') as fh:
         data = pickle.load(fh)
+
     graph = data['thermograph']['last_solution']['best_solution']
     for node_i, node_j, edge_data in graph.edges.data():
         edge_data['mcs'] = Chem.MolToSmiles(edge_data['mcs'])
+    map_info = {node_i+','+node_j: edge_data for node_i,
+                node_j, edge_data in graph.edges.data()}
+    datas = json.dumps(map_info, indent=2, separators=(',', ': '))
+    with open(map_info_file, 'w') as f:
+        f.write(datas)
 
-    map_info = {node_i + ',' + node_j: edge_data for node_i, node_j, edge_data in graph.edges.data()}
-    data = json.dumps(map_info, indent=2, separators=(',', ': '))
-    with open(file_path, 'w') as f:
-        f.write(data)
 
-def load_json(file_path):
-    with open(file_path, 'r') as f:
-        data = json.load(f)
-    return data
-def write_json(file_path):
-    path_info = {}
-    path_info['add_edge'] = [('FXR_76', 'FXR_84'), ('FXR_84', 'FXR_85')]
-    path_info['del_edge'] = [('FXR_12', 'FXR_84'), ('FXR_12', 'FXR_74'), ('FXR_12', 'FXR_88')]
-    data = json.dumps(path_info, indent=2, separators=(',', ': '))
-    with open(file_path, 'w') as f:
-        f.write(data)
 if __name__ == '__main__':
-
-    #file_path = 'modify_map_input.json'
-
-    #write_json(file_path)
-    parser = argparse.ArgumentParser(description='the path of check_ligands json file')
-    parser.add_argument('--file_path', '-f', help='where is your json file?',
-                        default='docs/tutorial01/check_ligs_input.json')
+    parser = argparse.ArgumentParser(
+        description='the path of check_ligands json file')
+    parser.add_argument('--file_path', '-f', help='json file defined the edge to be modified',
+                        default='modify_map_input.json')
+    process_user_input.add_argparse_global_args(parser)
+    arguments = process_user_input.read_options(
+        parser, unpack_section='modify_perturbation_map')
     args = parser.parse_args()
+
     try:
-        data = load_json(args.file_path)
+        with open(args.file_path, 'r') as f:
+            modify_data = json.load(f)
     except:
-        print('cannot find your json file')
+        print('cannot find your modify_map_input json file')
         exit()
-    #add_edge = [('FXR_76', 'FXR_84'), ('FXR_84', 'FXR_85')]
-    #del_edge = [('FXR_12', 'FXR_84'), ('FXR_12', 'FXR_74'), ('FXR_12', 'FXR_88')]
-    Modify_Map(data)
-    output_info_json()
+
+    Modify_Map(modify_data, arguments.progress_file, arguments.graph_file)
+    output_info_json(arguments.progress_file, arguments.map_info_file)
